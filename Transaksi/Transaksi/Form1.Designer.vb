0268356d8960ec6b9047bd762f9cec8a04b8fd2a
Erwdhan429 <58174490+Erwdhan429@users.noreply.github.com>
2019-12-04 16:12:59 +0800

    Add files via upload

---
+++ Transaksi/Transaksi/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lbl_saldo = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DTP_time = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(271, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplikasi Keluar Masuk Saldo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tanggal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(115, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jenis"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(115, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jumlah"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(115, 194)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Keterangan"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(474, 63)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(474, 104)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Edit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(474, 152)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Hapus"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(574, 68)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(74, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Saldo Utama :"
+        '
+        'lbl_saldo
+        '
+        Me.lbl_saldo.AutoSize = True
+        Me.lbl_saldo.Cursor = System.Windows.Forms.Cursors.Default
+        Me.lbl_saldo.Location = New System.Drawing.Point(588, 104)
+        Me.lbl_saldo.Name = "lbl_saldo"
+        Me.lbl_saldo.Size = New System.Drawing.Size(0, 13)
+        Me.lbl_saldo.TabIndex = 13
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 224)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(648, 220)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(115, 73)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(18, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "ID"
+        '
+        'DTP_time
+        '
+        Me.DTP_time.Location = New System.Drawing.Point(240, 36)
+        Me.DTP_time.Name = "DTP_time"
+        Me.DTP_time.Size = New System.Drawing.Size(200, 20)
+        Me.DTP_time.TabIndex = 18
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(240, 109)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(200, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(240, 73)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox1.TabIndex = 19
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(240, 152)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox2.TabIndex = 20
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(240, 191)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox3.TabIndex = 21
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(688, 507)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DTP_time)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.lbl_saldo)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lbl_saldo As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label7 As Label
+    Friend WithEvents DTP_time As DateTimePicker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ Transaksi/Transaksi/Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports System.Data.Odbc
+
+Public Class Form1
+    Public tampil As Integer
+    Public saldo As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        TampilGrid()
+        MunculCombo()
+        updateDisplay()
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress2(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        TextBox1.MaxLength = 7
+        If e.KeyChar = Chr(13) Then
+            bukakoneksi()
+            CMD = New OdbcCommand("Select * From transaksi where id ='" & TextBox1.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            If Not RD.HasRows Then
+                MsgBox("id tidak ada, Silahkan coba lagi!")
+                TextBox1.Focus()
+            Else
+                TextBox2.Text = RD.Item("jumlah")
+                TextBox3.Text = RD.Item("Keterangan")
+                DTP_time.Value = RD.Item("tanggal")
+                ComboBox1.Text = RD.Item("jenis")
+                TextBox1.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Silahkan Isi Semua Form")
+        Else
+            bukakoneksi()
+            If ComboBox1.Text = "Masuk" Then
+                saldo = CInt(TextBox2.Text)
+                tampil = tampil + saldo
+            ElseIf ComboBox1.Text = "Keluar" Then
+                saldo = CInt(TextBox2.Text)
+                tampil = tampil - saldo
+            End If
+
+            If tampil < 0 Then
+                tampil = tampil + saldo
+                MsgBox("data minus")
+            Else
+                Dim simpan As String = "Insert into transaksi values('" & DTP_time.Value & "','" & TextBox1.Text & "','" & ComboBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "')"
+
+                CMD = New OdbcCommand(simpan, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input data berhasil")
+            End If
+
+            updateDisplay()
+            TampilGrid()
+            KosongkanData()
+            tutupkoneksi()
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles Button2.Click
+
+        bukakoneksi()
+
+        Dim edit As String = "update transaksi set
+        jumlah ='" & TextBox2.Text & "',
+        keterangan ='" & TextBox3.Text & "',
+        tanggal ='" & DTP_time.Value & "',
+        jenis='" & ComboBox1.Text & "' where id='" & TextBox1.Text & "'"
+        CMD = New OdbcCommand(edit, CONN)
+        CMD.ExecuteNonQuery()
+        MsgBox("Data berhasil di Update")
+
+        TampilGrid()
+        KosongkanData()
+        tutupkoneksi()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Silahkan pilih data yang akan dihapus dengan masukkan ID dan Enter")
+        Else
+            If MessageBox.Show("Yakin akan dihapus ?", "", MessageBoxButtons.YesNo) = System.Windows.Forms.DialogResult.Yes Then
+                bukakoneksi()
+                Dim hapus As String = "delete From transaksi where id='" & TextBox1.Text & "'"
+                CMD = New OdbcCommand(hapus, CONN)
+                CMD.ExecuteNonQuery()
+                updateDisplay()
+                TampilGrid()
+                KosongkanData()
+                tutupkoneksi()
+            End If
+        End If
+    End Sub
+
+    Sub TampilGrid()
+
+        bukakoneksi()
+
+        DA = New OdbcDataAdapter("select * From transaksi", CONN)
+        DS = New DataSet
+        DA.Fill(DS, "transaksi")
+        DataGridView1.DataSource = DS.Tables("transaksi")
+
+        tutupkoneksi()
+    End Sub
+
+    Sub MunculCombo()
+        ComboBox1.Items.Add("Masuk")
+        ComboBox1.Items.Add("Keluar")
+    End Sub
+
+    Sub KosongkanData()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+
+    Private Sub updateDisplay()
+        bukakoneksi()
+        CMD = New OdbcCommand(" select * from transaksi order by id desc limit 1", CONN)
+        RD = CMD.ExecuteReader
+        RD.Read()
+        If Not RD.HasRows Then
+            lbl_saldo.Text = "Rp. 0"
+        Else
+            lbl_saldo.Text = "RP. " & RD.Item("saldo")
+            tampil = RD.Item("saldo")
+        End If
+        tutupkoneksi()
+    End Sub
+End Class
